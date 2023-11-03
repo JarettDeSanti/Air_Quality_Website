9d505170cc4ae1efcd5b8f7b0e693ed636e05396
snowlxy <snowlxy1202@gmail.com>
2023-11-02 23:11:42 -0700

    contact page styling and added images

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
-  console.log(elems);
   var instances = M.Sidenav.init(elems, 'left');
 });
