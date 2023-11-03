7a4a6cdab20f07d508dee8fdf513a7fe7d6018f1
snowlxy <snowlxy1202@gmail.com>
2023-11-03 12:41:22 -0700

    recommendation page sytling

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, 'left');
 });
+
+$(':radio').change(function() {
+  console.log('New star rating: ' + this.value);
+});
