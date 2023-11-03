0ab564afbf6bef622c82f351d3bba3f00eefc1a4
Jarett DeSanti <jarettdesanti@gmail.com>
2023-11-03 16:09:25 -0400

    added function javascript

--- javascript/script.js
+++ javascript/script.js
@@ -4,23 +4,42 @@ const openWeatherApiKey = "94e6bf67634acaba77df3c9e64af09de";
 
 
 
+$("#form").on("submit", function (event) {
+  event.preventDefault()
+  const city = $("#city").val()
+  console.log(city)
+  let form = event.target
+  const queryType = $(".search-button").children().each(function(event) {
+    console.log(event.target)
+    let buttonSelection = $(this).attr("id")
+    console.log(buttonSelection)
+  }) 
+  console.log(queryType)
+  // fetch ("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid="+openWeatherApiKey).then(function(response){
+  // if (response.ok) {
+  //   return response.json()
+  // }
+  // }).then(function(data){
+  //   console.log(data)
+  })
+
+  
+  // const searchPageUrl = "https://jarettdesanti.github.io/Air_Quality_Website/html/search.html";
+  // $(location).attr('href', searchPageUrl);
+// });
+
+
+
+// $("#form").on("click", function () {
+//   const searchPageUrl = "https://jarettdesanti.github.io/Air_Quality_Website/html/search.html";
+//   $(location).attr('href', searchPageUrl);
+// });
 
 
 
 
 
 
-$("#weather-button").on("click", function () {
-  const searchPageUrl = "https://jarettdesanti.github.io/Air_Quality_Website/html/search.html";
-  $(location).attr('href', searchPageUrl);
-});
-
-
-
-$("#air-quality-button").on("click", function () {
-  const searchPageUrl = "https://jarettdesanti.github.io/Air_Quality_Website/html/search.html";
-  $(location).attr('href', searchPageUrl);
-});
 
 
 
