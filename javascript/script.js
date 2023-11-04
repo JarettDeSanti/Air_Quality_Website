dd390bd2a7b26fa7191ff903c25cbcc33e81579b
Jarett DeSanti <jarettdesanti@gmail.com>
2023-11-04 15:49:56 -0400

    added functions for air quality data

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,12 @@
-const AIR_NOW_API_KEY = "81EABAEC-E22B-443C-B080-241D62F60DD8";
+const AIR_QUALITY_API_KEY = "303488b11b74e1026f10b3b907eced79";
 const OPEN_WEATHER_API_KEY = "94e6bf67634acaba77df3c9e64af09de";
 const WEATHER_BASE_PATH = "https://api.openweathermap.org/data/2.5/weather?q=";
-const container = $('#weather-results')
+const AIR_QUALITY_BASE_PATH = "http://api.openweathermap.org/geo/1.0/direct?q=";
+const containerWeather = $('#weather-results');
+const containerAirQuality = $('#air-quality-results');
 const cityInput = $('#city-input');
 const weatherButton = $('#weather-button');
+const airQualityButton = $('#air-quality-button');
 const toastHTML = '<span>Please enter a valid city!</span>';
 const cities = [];
 
@@ -32,11 +35,32 @@ const makeWeatherCard = (city, temperature, windspeed, humidity, description) =>
   `;
 }
 
+const makeAirQualityCard = (city) => {
+  const formattedCityName = city.toUpperCase()
+  return `
+    <div class="card">
+      <div class="card-content">
+        <p>Air Quality data for ${formattedCityName}</p>
+      </div>
+      <div class="card-tabs">
+        <ul class="tabs tabs-fixed-width">
+          <li class="tab"><a class="active" href="#air-quality">Air Quality Index (AQI)</a></li>
+        </ul>
+      </div>
+      <div class="card-content grey lighten-4">
+          
+      </div>
+    </div>
+  `;
+}
+
+
+
 const fetchData = city => {
   fetch(WEATHER_BASE_PATH + city + "&units=imperial&appid=" + OPEN_WEATHER_API_KEY).then(function (response) {
     if (response.ok) {
       return response.json()
-    }
+    } 
   }).then(function (data) {
     const temperature = data.main.temp;
     const windspeed = data.wind.speed;
@@ -44,11 +68,30 @@ const fetchData = city => {
     const description = data.weather[0].description;
 
     const weatherCard = makeWeatherCard(city, temperature, windspeed, humidity, description);
-    container.append(weatherCard);
+    containerWeather.append(weatherCard);
     const tabs = document.querySelector(".tabs");
     M.Tabs.init(tabs, {})
   })
 }
+
+
+const fetchDataAirQuality = city => {
+  fetch(AIR_QUALITY_BASE_PATH + city + "&appid=" + AIR_QUALITY_API_KEY)
+  .then(function (response) {
+    if (response.ok) {
+      return response.json()
+    } 
+  }).then(function (data) {
+    const airQuality = data.main;
+
+    const airQualityCard = makeAirQualityCard(city);
+    containerAirQuality.append(airQualityCard);
+    const tabs = document.querySelector(".tabs");
+    M.Tabs.init(tabs, {})
+  })
+}
+
+
 
 const getWeatherData = event => {
   event.preventDefault();
@@ -63,9 +106,23 @@ const getWeatherData = event => {
   }
 }
 
+const getAirQualityData = event => {
+  event.preventDefault();
+  const city = cityInput.val();
+
+  if (!city && !cities.includes(city.toUpperCase())) {
+    M.toast({ html: toastHTML });
+    return;
+  } else {
+    cities.push(city.toUpperCase());
+    fetchDataAirQuality(city);
+  }
+}
+
+
 
 weatherButton.on("click", getWeatherData);
-
+airQualityButton.on("click", getAirQualityData);
 
 
 
